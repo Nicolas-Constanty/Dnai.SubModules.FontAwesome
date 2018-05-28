2265d82e5795a16d8c2214e855369bc5c610b9f6
Nicolas Constanty <nicolas.constanty@epitech.eu>
2018-05-29 00:41:30 +0200

    load from plugin

--- dnaifontawesome_plugin.cpp
+++ dnaifontawesome_plugin.cpp
@@ -8,13 +8,13 @@ void DnaiFontAwesomePlugin::registerTypes(const char *uri)
 {
     // @uri com.saltystudio.fontawesomecomponent
     Q_ASSERT(uri == QLatin1String("Dnai.FontAwesome"));
-//    auto id = QFontDatabase::addApplicationFont(":/fa5.otf");
+    auto id = QFontDatabase::addApplicationFont(":/fa5.otf");
 //    QString family = QFontDatabase::applicationFontFamilies(id).at(0);
 //    qDebug() << family;
-//    id = QFontDatabase::addApplicationFont(":/faBrand5.otf");
+    id = QFontDatabase::addApplicationFont(":/faBrand5.otf");
 //    family = QFontDatabase::applicationFontFamilies(id).at(0);
 //    qDebug() << family;
-//    id = QFontDatabase::addApplicationFont(":/faSolid5.otf");
+    id = QFontDatabase::addApplicationFont(":/faSolid5.otf");
 //    family = QFontDatabase::applicationFontFamilies(id).at(0);
 //    qDebug() << family;
 }
